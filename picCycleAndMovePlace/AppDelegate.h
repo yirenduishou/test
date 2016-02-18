a0778d2d1e43b09043ecf800456e599ede5d3147
Yirenduishou <zJob_shuang@163.com>
2016-02-18 17:14:59 +0800

    Initial Commit

---
+++ picCycleAndMovePlace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  picCycleAndMovePlace
+//
+//  Created by SkyAndSea on 16/2/18.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ picCycleAndMovePlace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  picCycleAndMovePlace
+//
+//  Created by SkyAndSea on 16/2/18.
+//  Copyright © 2016年 SkyAndSea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
